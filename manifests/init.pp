dfe7fc46e8d1b86e096b2b4c9af7fc2bb77cb4d3
Rasmusekmanhh <31437190+Rasmusekmanhh@users.noreply.github.com>
2017-12-05 13:34:49 +0000

    Add files via upload

---
+++ manifests/init.pp
@@ -0,0 +1,36 @@
+class student { 
+	package { 'libreoffice':
+		ensure => 'installed',
+		allowcdrom => true,
+	}
+	
+	package { 'vlc':
+                ensure => 'installed',
+                allowcdrom => true,
+        }
+
+	package { 'geogebra':
+                ensure => 'installed',
+                allowcdrom => true,
+        }
+	
+	package { 'gimp':
+                ensure => 'installed',
+                allowcdrom => true,
+        }
+
+	package { 'audacity':
+                ensure => 'installed',
+                allowcdrom => true,
+        }
+
+	package { 'libreoffice-l10n-fi':
+		ensure => 'installed',
+		allowcdrom => true,
+	}
+
+	file { '/etc/firefox/ssyspref.conf':
+		content => template('student/ssyspref.conf'),
+		require => Package['firefox'],
+	}
+}
---
+++ templates/ssyspref.js
@@ -0,0 +1,4 @@
+user_pref("browser.startup.homepage", "https://en.wikipedia.org/wiki/Main_Page");
+lockPref("browser.startup.homepage", "https://en.wikipedia.org/wiki/Main_Page");
+
+
